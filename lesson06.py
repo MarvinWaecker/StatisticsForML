4c392e78bedc707c85250a44e105fb21eeaf76e5
CodeKalb <marvin.waecker@icloud.com>
2022-04-11 18:45:56 +0200

    lesson6

---
+++ lesson06.py
@@ -0,0 +1,11 @@
+# calculate the confidence interval
+from statsmodels.stats.proportion import proportion_confint
+# calculate the interval
+lower, upper = proportion_confint(88, 100, 0.05)
+print('lower=%.3f, upper=%.3f' % (lower, upper))
+
+# Effect size calculation methods
+# - pearson
+# - spearmen
+# - r^2
+# - Cohen's d
